975462b66180f85e466b1bee066c3be8f83063ee
Francesca Barletta <wallebar@gmail.com>
2024-05-22 18:02:23 +0200

    add margin in card

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -9,7 +9,7 @@
     <div class="row justify-content-between ">
 @foreach($comics as $comic)
 
-        <div class="card" style="width: 18rem;">
+        <div class="card m-2" style="width: 18rem;">
              <img  style="width: 100%; height: 300px; object-fit:cover;" src="{{ $comic['thumb'] }}" class="card-img-top" alt="...">
              <div class="card-body">
                  <p class="card-text">{{ $comic['title'] }}</p>
